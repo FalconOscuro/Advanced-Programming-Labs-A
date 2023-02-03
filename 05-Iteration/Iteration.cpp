dec297875ea2454b666dca47a1f2cab76c06030e
Peter Garrod <p.jlgarrod@gmail.com>
2023-02-03 14:47:49 +0000

    Input parsing

--- 05-Iteration/Iteration.cpp
+++ 05-Iteration/Iteration.cpp
@@ -1,16 +1,28 @@
 #include <iostream>
+#include <limits> // numeric_limits
+
 using namespace std;
+
+template <typename T> T ParseInput(const char* msg)
+{
+    T out;
+    while(!(cout << msg, cin >> out))
+    {
+        // If input fails input buffer needs to be reset to avoid infinite loop
+        cin.clear();
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+        cout << "Invalid value please try again." << endl;
+    }
+    return out;
+}
 
 int main(int argc, char* argv[])
 {
     cout << "----Factorial Finder----" << endl;
-    cout << "Input a number: ";
 
-    int n;
-    cin >> n;
+    int n = ParseInput<int>("Input a number: ");
 
     int result = 1;
-
     for (int i = 2; i <= n; i++)
         result *= i;
     

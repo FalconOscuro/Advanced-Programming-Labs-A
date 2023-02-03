18229f3248dee5448f8f779b367b79057e0c7f18
Peter Garrod <p.jlgarrod@gmail.com>
2023-02-03 13:27:47 +0000

    Added input parsing

--- 02-Temperature/Temperature.cpp
+++ 02-Temperature/Temperature.cpp
@@ -1,17 +1,20 @@
 #include <iostream>
+#include <limits>
 
 using namespace std;
 
 int main()
 {
-    cout << "----Farenheight to Celcius----\n"
-        << "Please enter the value to convert:";
+    cout << "----Farenheight to Celcius----" << endl;
 
-    string tempStr;
-    cin >> tempStr;
-
-    float fH = stof(tempStr.c_str());
-    // Need to check for std::invalid_argument & std::out_of_range
+    float fH;
+    while (!(cout << "Please enter the value to convert:", cin >> fH))
+    {
+        // If input fails input buffer needs to be reset to avoid infinite loop
+        cin.clear();
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+        cout << "Invalid value please try again." << endl;
+    }
 
     float c = 5.F/9.F * (fH - 32);
     cout << "Converted to Celcius that is " << c << " degrees." << endl;

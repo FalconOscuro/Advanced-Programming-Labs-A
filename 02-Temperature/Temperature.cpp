10a4e51205062c9977d41a4eb9e93632a70a0e5a
Peter Garrod <p.jlgarrod@gmail.com>
2023-02-03 14:45:49 +0000

    Moved input parsing to own function

--- 02-Temperature/Temperature.cpp
+++ 02-Temperature/Temperature.cpp
@@ -3,19 +3,28 @@
 
 using namespace std;
 
-int main()
+/// @brief Loops until input is accepted
+/// @tparam T input type
+/// @param msg Message posted when requesting input
+/// @return parsed input
+template <typename T> T ParseInput(const char* msg)
 {
-    cout << "----Farenheight to Celcius----" << endl;
-
-    float fH;
-    while (!(cout << "Please enter the value to convert:", cin >> fH))
+    T out;
+    while(!(cout << msg, cin >> out))
     {
         // If input fails input buffer needs to be reset to avoid infinite loop
         cin.clear();
         cin.ignore(numeric_limits<streamsize>::max(), '\n');
         cout << "Invalid value please try again." << endl;
     }
+    return out;
+}
 
+int main()
+{
+    cout << "----Farenheight to Celcius----" << endl;
+
+    float fH = ParseInput<float>("Please enter the value to convert:");
     float c = 5.F/9.F * (fH - 32);
     cout << "Converted to Celcius that is " << c << " degrees." << endl;
 }
